e29f23ff0ba9924f14b0e8bb494d7d2cde0173ad
Ariel Silva Jr <ceosilvajr@gmail.com>
2015-08-02 20:03:51 +0800

    Initial Commit

---
+++ app/src/main/java/com/ceosilvajr/app/onlineshoppingcart/MainActivity.java
@@ -0,0 +1,52 @@
+package com.ceosilvajr.app.onlineshoppingcart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ceosilvajr.app.onlineshoppingcart.objects.User;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        User user = new User();
+        user.setName("Ariel Q. Silva Jr");
+        user.setImageDrawable(R.drawable.ic_avatar);
+
+        TextView tvName =(TextView) findViewById(R.id.my_name);
+        tvName.setText(user.getName());
+
+        ImageView ivImage = (ImageView) findViewById(R.id.avatar);
+        ivImage.setImageResource(R.drawable.ic_avatar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/ceosilvajr/app/onlineshoppingcart/objects/User.java
@@ -0,0 +1,27 @@
+package com.ceosilvajr.app.onlineshoppingcart.objects;
+
+/**
+ * Created by ceosilvajr on 8/1/15.
+ */
+public class User {
+
+    String name;
+
+    int imageDrawable;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImageDrawable() {
+        return imageDrawable;
+    }
+
+    public void setImageDrawable(int imageDrawable) {
+        this.imageDrawable = imageDrawable;
+    }
+}
